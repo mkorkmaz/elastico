f2feb8e9ad3831d18062092b004f1f11a1614962
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2016-05-04 10:15:47 +0300

    Fixes a deletion issue when doc_id does not exists
    If you try to delete a document with an _id that does not exist, $ES->delete throws an Exception. B/c of default behaviour of reformo/rslim, it bypasses all Exceptions and it returns Exception message. In this case it returns this message as a plain text json that elasticsearch-php returns.

--- apps/elastico/controllers/es/doc.delete.php
+++ apps/elastico/controllers/es/doc.delete.php
@@ -7,13 +7,23 @@ function app_content($request, $args) {
     $index  = $args['index'];
     $type   = $args['type'];
     $id     = $args['id'];
-
     $params = [];
     $params['index'] = $index;
     $params['type'] = $type;
     $params['id'] = $id;
-    $results = $ES->delete($params);
+    try{
+        $result = $ES->delete($params);
+    }
+    catch (Exception $e){
+        /*
+        If you try to delete a document with an _id that does not exist, $ES->delete throws an Exception.
+        B/c of default behaviour of reformo/rslim, it bypasses all Exceptions and it returns Exception message.
+        In this case it returns this message as a plain text json that elasticsearch-php returns.
+        */
+        $result = json_decode($e->getMessage(), true);
+    }
+    $found = ($result['found'] === true) ? 1 : 0;
     sleep(1);
-    header("location:" . BASE_HREF . "/" . $index . "/" . $type . "?res=success&req=delete");
+    header("location:" . BASE_HREF . "/" . $index . "/" . $type . "?res=success&req=delete&f=".$found);
     exit;
 }
