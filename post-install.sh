1f13af99d9591634530df44c3263545cf1957fd5
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2016-05-05 23:34:25 +0300

    composer config updated

--- post-install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cp -r vendor/mkorkmaz/elastico/app ./app
-cp -r app/app_config.sample.php app/app_config.php
--- post-update.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cp -r vendor/mkorkmaz/elastico/app ./app
