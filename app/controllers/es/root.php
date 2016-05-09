ead0eba7c5c06535013033ae29140705423be8c8
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2016-05-09 21:51:17 +0300

    es_root refined

--- app/controllers/es/root.php
+++ app/controllers/es/root.php
@@ -4,24 +4,19 @@ function es_root($request, $args)
 {
     global $ESConn;
 
-    $indices = $ESConn->indices()->getAliases();
+    $indices = array_keys($ESConn->indices()->getAliases());
     $indexes = [];
     $params = [];
-    foreach ($indices as $index => $alieses) {
+    foreach ($indices as $index) {
         if (strpos($index, '.') !== 0) {
             $params['index'] = $index;
             $index_data = [];
             $index_data['name'] = $index;
             $index_info = $ESConn->indices()->getMapping($params);
-            foreach ($index_info[$index]['mappings'] as $type => $type_info) {
-                if ($type == '_default_') {
-                    continue;
-                }
-                $index_data['types'][] = $type;
-            }
+            $index_data['types'] = array_keys($index_info[$index]['mappings']);
             $indexes[] = $index_data;
         }
     }
 
-    return ['data' => ['indexes' => $indexes], 'app_title' => 'Indexes'];
+    return ['data' => ['indexes' => $indexes], 'app_title' => 'Indices'];
 }
